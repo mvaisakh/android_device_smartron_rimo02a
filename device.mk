fde6e9bad3563ea44ff99bffc208246caef1b445
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-12 00:54:00 +0530

    rimo02a: Add dummy android.hidl.manager@1.0
    Change-Id: I91d2f69243489ca72cab381b0e4dfc3dc1a1abff

--- device.mk
+++ device.mk
@@ -120,7 +120,8 @@ PRODUCT_COPY_FILES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
 
 # Media
 PRODUCT_COPY_FILES += \
