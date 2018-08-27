71e53dfa2efaa5f384e2245ecab7698fbacd8900
Vachounet <vachounet@live.fr>
2018-08-27 17:01:01 +0000

    rimo02a: HIDL: Build Fingerprint and Gatekeeper HAL
    Signed-off-by: Vachounet <vachounet@live.fr>
    Signed-off-by: Vaisakh Murali <mvaisakh@protonmail.com>

--- device.mk
+++ device.mk
@@ -274,3 +274,12 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.graphics.composer@2.1-impl
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.0-service-custom
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
