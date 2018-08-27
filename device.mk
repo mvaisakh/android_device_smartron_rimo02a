9294bf86ae734638c1e1d4b4ecff82952eb9e8a7
Jesse Hall <jessehall@google.com>
2018-08-27 18:40:23 +0000

    rimo02a: Declare Vulkan compute level 0 system feature
    Bug: 38396207 (VK_KHR_variable_pointers)
    Bug: 64982680 (maxPerStageDescriptorStorageBuffers >= 16)
    Test: CtsGraphicsTestCases:.VulkanFeaturesTest (ag/2810565)
    Change-Id: I61cde61f53f0e9a1c657e6bd6658a4393360b0a6
    
    Signed-off-by: Vaisakh Murali <mvaisakh@protonmail.com>

--- device.mk
+++ device.mk
@@ -73,6 +73,7 @@ PRODUCT_COPY_FILES += \
 # Display
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:system/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
 
